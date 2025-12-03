e022fd76425c142d66e3e0fe05e5ef3f766fbdfc
Harsh Karan <harshkaranofficial@gmail.com>
2025-12-03 20:03:52 +0530

    Arrays

---
+++ 07_Arrays/20_vectorAtSort.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    vector<int>V;
+    V.push_back(6);
+    V.push_back(1);
+    V.push_back(19);
+    V.push_back(100);
+    //v.at(2) = 99;
+    // count<<v.at(2);
+    for(int i=0 ; i<V.size() ; i++){
+        cout<<V.at(i)<<" ";
+    }
+    cout<<endl;
+
+// sort
+
+sort(V.begin(),V.end());
+    for(int i=0 ; i<V.size();i++){
+        cout<<V.at(i)<<" ";
+    }
+    cout<<endl;
+
+}   
