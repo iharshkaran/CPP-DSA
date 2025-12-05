9eba2a02bbc645cbc39bdc087dc189043f5647b8
Harsh Karan <harshkaranofficial@gmail.com>
2025-12-05 22:00:00 +0530

    Arays

---
+++ 07_Arrays/25_RevOrderWithoutExtraArray.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void display(vector<int>& a){
+        for(int i=0;i<a.size();i++){
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
+    }
+int main(){
+    vector<int> v;
+    int n;
+    cout<<"Enter array size : ";
+    cin>>n;
+    for(int i=0;i<n ; i++){
+        int q;
+        cin>>q;
+        v.push_back(q);
+    }
+    int i = 0;
+    int j = v.size()-1; 
+    while(i<=j){
+        //swap v[i] and v[j]
+        int temp = v[i];
+        v[i] = v[j];
+        v[j]= temp;
+        i++;
+        j--;
+    }
+display(v);
+
+
+
+}
